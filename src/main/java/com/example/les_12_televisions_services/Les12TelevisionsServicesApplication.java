71c024a74bd613265004619a869a3ef5177f7fe0
Nastyplayer <jftalavera@hotmail.com>
2023-03-13 23:14:07 +0100

    init

---
+++ src/main/java/com/example/les_12_televisions_services/Les12TelevisionsServicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.les_12_televisions_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Les12TelevisionsServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Les12TelevisionsServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/les_12_televisions_services/Les12TelevisionsServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.les_12_televisions_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Les12TelevisionsServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
